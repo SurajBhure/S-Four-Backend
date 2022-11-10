4c3456bdfe8b25260b39a224d55e53c55a80342c
Suresh Khairnar <sureshkhairnar39@gmail.com>
2022-11-10 12:38:51 +0530

    verify token method created

--- helpers/tokenServices.js
+++ helpers/tokenServices.js
@@ -1,10 +1,10 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config/envConfig");
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config/envConfig')
 
+module.exports.createToken = (user) => {
+  return jwt.sign(user, JWT_SECRET, { expiresIn: '7d' })
+}
 
-module.exports.createToken = (user)=>{
-    return jwt.sign(
-        user,
-        JWT_SECRET,{expiresIn:'7d'}
-      );
-} 
+module.exports.verifyToken = (token) => {
+  return jwt.sign(token, JWT_SECRET)
+}
